46371dab39581c57d69c98107c7c34a8939e3a4c
BillyBoy26 <benjamin.scheinhardt@gmail.com>
2016-05-28 16:52:52 +0200

    bk-toolbar récupère toute seul ses données

--- controllers/authentification/authentication.js
+++ controllers/authentification/authentication.js
@@ -3,5 +3,11 @@ var router = require('express').Router();
 
 router.use("/facebook", require('./facebookAuth.js'));
 
-
+router.get('/getConnectData', function(req, res) {
+    res.contentType("json");
+    res.send({
+        isAuthenticated: req.isAuthenticated(),
+        user :req.user
+    });
+});
 module.exports = router;
